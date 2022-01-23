51ff68916abf22c876029880ce495ce4c2738a21
Raxcu <Raxcuz@gmail.com>
2022-01-23 14:29:46 +0200

    the match function is nice

--- AoC/Day 2.py
+++ AoC/Day 2.py
@@ -1,39 +1,41 @@
 with open('AoC\Sources\Input2.txt', 'r') as raw:
     Instr = [x.strip().split(' ') for x in raw.readlines()]
-    Dir, val = [x[0] for x in Instr], [int(x[1]) for x in Instr]
+
 
 def part1():
-    
-    xval = 0
     yval = 0
+    xval = 0
 
-    for i, x in enumerate(Dir):
+    for instruction in Instr:
+        match instruction[0]:
+            case 'down':
+                yval += int(instruction[1])
+            case 'up':
+                yval -= int(instruction[1])
+            case 'forward':
+                xval += int(instruction[1])
 
-        if 'down' in x: yval += val[i]
+    print(f"\nPart 1\nanswer is: {xval * yval}\n")
 
-        elif 'up' in x: yval -= val[i]
-
-        else: xval += val[i]
-
-    print(f"\nPart 1\nanswer is: {xval*yval}\n")
 
 def part2():
-    
     xval = 0
     yval = 0
     aim = 0
 
-    for i, x in enumerate(Dir):
+    for instruction in Instr:
+        dirval = int(instruction[1])
+        match instruction[0]:
+            case 'down':
+                aim += dirval
+            case 'up':
+                aim -= dirval
+            case 'forward':
+                xval += dirval
+                yval += aim * dirval
 
-        if 'down' in x: aim += val[i]
+    print(f"Part 2\nanswer is: {xval * yval}\n")
 
-        elif 'up' in x: aim -= val[i]
-
-        else: 
-            xval += val[i] 
-            yval += aim * val[i]
-    
-    print(f"Part 2\nanswer is: {xval*yval}\n")
 
 part1()
 part2()
